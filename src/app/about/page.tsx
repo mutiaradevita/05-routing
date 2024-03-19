7444c484cbb1c1d5e4122a7bc64da5a223bbc980
mutiaradevita <mutiaradevitaekaputri>
2024-03-19 15:55:38 +0700

    praktikum 1

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+    return ( 
+        <div>
+            <h1>About Me</h1> 
+            <p>Nama     : Mutiara Devita Eka Putri</p>
+            <p>NIM      : 2141720135</p>
+            <p>Kelas    : TI 3A</p>
+            <p>Absen    : 15</p>
+        </div>
+    );
+  }
