2dbe5f3451aa27cd4b325753361735ee83980520
mutiaradevita <mutiaradevitaekaputri>
2024-03-19 16:07:42 +0700

    praktikum 2

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+          <h1>Blog</h1>
+    );
+  }
+
+  // Pertanyaan : Apa kekurangan yang mungkin terjadi jika menggunakan pendekatan pada Praktikum 2 untuk menangani routing?
+  // Jawab      : Pendekatan bersarang dalam routing memang mudah untuk navigasi sederhana, namun memiliki kekurangan seperti sulitnya memelihara struktur kompleks, 
+  //              kurang fleksibel untuk navigasi dinamis, URL yang tidak jelas, dan SEO yang terhambat. 
