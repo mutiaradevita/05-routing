f82df635675e000db8f21317fdc2bc2ebce7e277
mutiaradevita <mutiaradevitaekaputri>
2024-03-20 13:08:26 +0700

    praktikum 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-    return <h1>Welcome to Home</h1> ;
-  }
+  return (
+        <div>
+          <h1>Welcome to Home</h1>
+          <ul>
+                <li><Link href='/about'>About</Link></li>
+                <li><Link href='/blogs'>Blog</Link></li>
+                <li><Link href='/products'>Products</Link></li>
+            </ul>
+        </div>
+  );
+}
   
